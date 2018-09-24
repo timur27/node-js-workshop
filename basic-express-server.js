d7a63cdf49a446981afb2e583f9fddbf1a76cfb3
Timur <timur.karimow.95@gmail.com>
2018-09-24 15:34:31 +0200

    Added static data

--- basic-express-server.js
+++ basic-express-server.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 app.listen('8080', () => console.log('Listen on 8080'));
+app.use(express.static('public'));
 
 // First version of GET //
-// app.get('/', (req, res) => {
-//     res.send('Congrats! You are done with your GET request');
-// });
+app.get('/', (req, res) => {
+    res.send({
+        message: 'hello express json answer',
+    });
+});
 
 // Second version of GET //
-app.get('/', (req, res) => {
-    res.send('halo Timka');
+// app.get('/', (req, res) => {
+//     res.send('halo Timka');
+// });
+
+app.post('/', (req, res) => {
+    console.log('We are now in POST');
+    res.redirect('/');
 });
 
